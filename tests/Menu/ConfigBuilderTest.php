c4df5dd0ba8232920d49875a23bf4f08c00a40c7
core23 <mail@core23.de>
2025-02-07 07:51:28 +0100

    Remove psalm in favor of phpstan

--- tests/Menu/ConfigBuilderTest.php
+++ tests/Menu/ConfigBuilderTest.php
@@ -374,7 +374,6 @@ final class ConfigBuilderTest extends TestCase
     protected function withParameter(InvokedCount $matcher, array $parameters): Closure
     {
         return static function () use ($matcher, $parameters): void {
-            /** @psalm-suppress InternalMethod */
             $callNumber = $matcher->numberOfInvocations();
 
             Assert::assertEquals($parameters[$callNumber-1], \func_get_args(), sprintf('Call %s', $callNumber));
